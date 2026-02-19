ad68f6b1ed7e09b01cf08f3d166dedd917bea435
Abc Develop <abcdev.ptk@gmail.com>
2026-02-19 16:38:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1579760205-cb7cc",
+  "appId": "1:910160252803:web:24ba6d28eefe7ec91b30b6",
+  "apiKey": "AIzaSyDAoacubRZnbcrOgEFs2Ambhox1CNbK4cw",
+  "authDomain": "studio-1579760205-cb7cc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "910160252803"
+};
