df123f54d3a821fa1060935e0eb241ea0972d034
Abc Develop <abcdev.ptk@gmail.com>
2026-02-20 04:18:30 +0000

    i see that the problem, the features listed on plans creation are no exi

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,16 +4,20 @@ export function QordiaLogo({ className, ...props }: React.SVGProps<SVGSVGElement
   return (
     <svg
       viewBox="0 0 100 100"
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="6"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={cn(className)}
       {...props}
     >
-      <path
-        d="M50,10A40,40,0,1,0,90,50,40,40,0,0,0,50,10ZM50,82A32,32,0,1,1,82,50,32,32,0,0,1,50,82Z"
-        opacity="0.3"
-      />
-      <path d="M43,35h14v14H43V35Zm7-18a2,2,0,0,0-2,2V28H39a2,2,0,0,0-2,2v22a2,2,0,0,0,2,2H48v9a2,2,0,0,0,4,0V54h9a2,2,0,0,0,2-2V30a2,2,0,0,0-2-2H52V19a2,2,0,0,0-2-2Zm7,30H41V32h8v7a2,2,0,0,0,4,0V32h6V50Z" />
-      <path d="M70,64a6,6,0,1,0,6,6A6,6,0,0,0,70,64Zm0,8a2,2,0,1,1,2-2A2,2,0,0,1,70,72Z" />
+      <path d="M50 90C27.9086 90 10 72.0914 10 50C10 27.9086 27.9086 10 50 10C72.0914 10 90 27.9086 90 50" />
+      <path d="M50 90C66.5685 90 80 76.5685 80 60C80 43.4315 66.5685 30 50 30" />
+      <path d="M35 35h15v15H35z" fill="currentColor" stroke="none" />
+      <path d="M35 55h15" />
+      <path d="M55 35h10" />
+      <path d="M55 45h10v10H55z" fill="currentColor" stroke="none" />
     </svg>
   );
 }
