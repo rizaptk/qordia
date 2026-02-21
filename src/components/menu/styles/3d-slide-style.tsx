e4c8d855b597eea83d3c686b07f5f0163c38faef
Abc Develop <abcdev.ptk@gmail.com>
2026-02-21 17:46:25 +0000

    need quick fix : ## Error Type Runtime TypeError
    ## Error Message
    loopI

--- src/components/menu/styles/3d-slide-style.tsx
+++ src/components/menu/styles/3d-slide-style.tsx
@@ -37,14 +37,14 @@ export function ThreeDSlideStyle({ menuItems, onSelectItem }: StyleProps) {
         const engine = emblaApi.internalEngine()
         const scrollProgress = emblaApi.scrollProgress()
         const slidesInView = emblaApi.slidesInView()
+        const isScrollEvent = eventName === 'scroll'
 
         emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
             let diffToTarget = scrollSnap - scrollProgress
-            const isSlideInView = slidesInView.indexOf(snapIndex) > -1
-
+            
             if (engine.options.loop) {
                 engine.slideLooper.loopPoints.forEach((loopItem) => {
-                    const target = loopItem.target().get()
+                    const target = loopItem.target
                     if (snapIndex === loopItem.index && target !== 0) {
                         const sign = Math.sign(target)
                         if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress)
